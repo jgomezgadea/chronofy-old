4babc588e01a815e7f41d4b7898066c8dc5fbfed
Jose <josegomezgadea@gmail.com>
2017-08-26 20:14:47 +0200

    .

--- app/src/main/java/com/chronofy/android/chronofy/Fragment/mainFragment.java
+++ app/src/main/java/com/chronofy/android/chronofy/Fragment/mainFragment.java
@@ -2,45 +2,69 @@ package com.chronofy.android.chronofy.Fragment;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.chronofy.android.chronofy.R;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class mainFragment extends Fragment {
 
+    ListView mainListView = null; // Variable con la que referenciamos a la ListView principal
+    FloatingActionButton fab = null; // Variable con la que referenciamos al botón de añadir bricks
 
     public mainFragment() {
         // Required empty public constructor
     }
 
+    // Creamos un ArrayList de ejemplo que en un futuro serán los bricks
+    String[] ejemplo = new String[] { "Android", "iPhone", "WindowsMobile",
+            "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+            "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X" };
+    ArrayList<String> listaEjemplo = new ArrayList<>(Arrays.asList(ejemplo));
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        View res = inflater.inflate(R.layout.fragment_main, container, false);
+        final View res = inflater.inflate(R.layout.fragment_main, container, false);
 
-        ListView lv = (ListView) res.findViewById(R.id.mainListView);
+        // Ponemos el ListView principal en una variable
+        mainListView = res.findViewById(R.id.mainListView);
 
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
-                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
-                "Android", "iPhone", "WindowsMobile" };
-        List<String> list = Arrays.asList(values);
-        lv.setAdapter(new ArrayAdapter<String>(res.getContext(), android.R.layout.simple_list_item_1, list));
+        // El ArrayAdapter es lo que define el formato de la ListView y el ArrayList del que lee
+        // TODO Tengo que hacer un adaptador del tipo que yo quiero
+        final ArrayAdapter aa = new ArrayAdapter<>(res.getContext(), android.R.layout.simple_list_item_1, listaEjemplo);
+        mainListView.setAdapter(aa);
+
+        // TODO Este botón agregará un elemento a la ListView de tipo Brick
+        fab = res.findViewById(R.id.fab);
+        // Añadimos un listener a dicho botón
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast elementoAnyadido = Toast.makeText(res.getContext(), R.string.elemento_anyadido, Toast.LENGTH_SHORT);
+                // Añadimos el elemento
+                // TODO Esto en un futuro se hará desde otra ventana donde elegiremos el tipo
+                listaEjemplo.add("Juanito");
+                aa.notifyDataSetChanged();
+                // Mostramos el toast
+                elementoAnyadido.show();
+            }
+        });
+
         return res;
     }
 
--- app/src/main/java/com/chronofy/android/chronofy/MainActivity.java
+++ app/src/main/java/com/chronofy/android/chronofy/MainActivity.java
@@ -1,12 +1,10 @@
 package com.chronofy.android.chronofy;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -33,22 +31,12 @@ public class MainActivity extends AppCompatActivity
         fm = getSupportFragmentManager();
         fm.beginTransaction().replace(R.id.content_main, f).commit();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
 
-
-
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -89,7 +77,7 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
